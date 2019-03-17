63f890ff2e2c0a411d9244be8884c29da8c6f508
Ivan <jezhische@gmail.com>
2019-03-17 23:35:49 +0200

    intermediate

---
+++ src/main/java/com/jezh/textsaver/configuration/SecurityConfig.java
@@ -0,0 +1,82 @@
+package com.jezh.textsaver.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final DataSource dataSource;
+
+    @Value("${spring.queries.user-query}")
+    private String userQuery;
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
+    @Autowired
+    public SecurityConfig(BCryptPasswordEncoder bCryptPasswordEncoder, DataSource dataSource) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .usersByUsernameQuery(userQuery)
+                .authoritiesByUsernameQuery(rolesQuery)
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.
+                authorizeRequests()
+                .antMatchers("/login", "/sign-up", "/sign-in", "/access-denied").permitAll()
+//                .antMatchers("/product-images-uploads/**").hasAuthority("ADMIN")
+                .antMatchers("/**").hasAnyAuthority("ADMIN", "CUSTOMER")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/sign-in")
+                .failureUrl("/login?error=true")
+                // controller method GET mapped "/", and THE ADDRESS WILL BE http://localhost:8082/textsaver
+                .defaultSuccessUrl("/")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .and()
+                .logout()
+                // to use POST "/logout":
+//                .logoutUrl("/logout")
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied")
+        ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+//  let Spring knows that some static resources can be served skipping the antMatchers defined
+                .ignoring()
+                .antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
+}
--- src/main/java/com/jezh/textsaver/configuration/WebMvcConfig.java
+++ src/main/java/com/jezh/textsaver/configuration/WebMvcConfig.java
@@ -1,13 +1,26 @@
 package com.jezh.textsaver.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/index").setViewName("index");
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/").setViewName("index.html"); // "forward:/index.html"
+        registry.addViewController("/login").setViewName("login.html");
+//        registry.addViewController("/sign-in").setViewName("redirect:/");
+        registry.addRedirectViewController("/sign-in", "/");
+        registry.addViewController("/sign-up").setViewName("registration.html");
+        registry.addViewController("/access-denied").setViewName("access-denied.html");
     }
 }
